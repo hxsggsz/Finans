fbfc1ada0dcd883cd4e24dbe1afdc6ce24dfa91c
Victor Hugo <vt.hugo.2021@gmail.com>
2022-10-04 00:04:55 -0300

    new Header changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image"
 import Link from "next/link"
+import Image from "next/image"
+import { HeaderStyle, Picture, Section} from "."
 import logo from "../../assets/pictures/logo.png"
-import { Container } from "./Container"
-
+import mulher from "../../assets/pictures/capa-mulher.png"
 
 export const Header: React.FC = () => {
    return (
-      <Container>
-         <div>
+      <HeaderStyle>
             <nav>
                   <Link href='/'><a><Image alt='logo' src={logo} /></a></Link>
                <ul>
@@ -15,7 +14,14 @@ export const Header: React.FC = () => {
                   <a href='https://www.github.com/hxsggsz' target='_blank' ><li>GitHub</li></a>
                </ul>
             </nav>
-         </div>
-      </Container>
+
+            <Section>
+               <h1>Suas contas, descomplicadas</h1>
+               <p>Usado por mais de 1 Milhão de pessoas, o Finans é uma ferramenta online para facilitar a sua vida <span>Finan</span>ceira.</p>
+               <Picture>
+                  <Image alt="mulher" src={mulher}/>
+               </Picture>
+            </Section>
+      </HeaderStyle>
    )
 }
--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import Image from 'next/image';
 
 
-export const Container = styled.section`
+export const HeaderStyle = styled.header`
    width: 100%;
-   height: 100vh;
+   height: 95vh;
    background: #F5BB00;
    
 nav, ul {
@@ -24,14 +25,13 @@ li {
 
 li::before {
    content: "";
-   top: 65px;
+   top: 60px;
    right: 100px;
    width: 100px;
    height: 3px;
    background: currentColor;
    margin: 0 auto;
    position: absolute;
-   display: block;
    transform: scaleX(0);
    transition: transform 250ms ease; 
 }
@@ -40,8 +40,42 @@ li:hover::before {
    width: 100%;
    height: 3px;
    margin: 0 auto;
-   background: currentColor;
    display: block;
    transform: scaleX(2.5);
 }
+`;
+
+export const Section = styled.section`
+   h1, p {
+      color: #f5f5f5;
+      font-size: 50px;
+
+      top: 200px;
+      margin: auto 3rem;
+      display: block;
+      position: absolute;
+   }
+   h1 {
+      width: 30%;
+   }
+   p {
+      top:350px;
+      width: 45%;
+      
+      font-size: 30px;
+   }
+
+   span {
+      color: #DCDCDC;
+      font-style: italic;
+      font-size: 32px;
+   }
+`;
+
+export const Picture = styled.image`
+      width: 100%;
+      
+      bottom: 15px;
+      left: 45rem;
+      position: absolute;
 `;
